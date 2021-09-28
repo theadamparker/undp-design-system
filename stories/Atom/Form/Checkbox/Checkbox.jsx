b49fe9bca2d8e9677998fd40e8177ee79d451e5c
Shubham-Sharma-77 <shubham.sharma@srijan.net>
2021-09-28 12:27:53 +0530

    Feedbacks

--- stories/Atom/Form/Checkbox/Checkbox.jsx
+++ stories/Atom/Form/Checkbox/Checkbox.jsx
@@ -3,9 +3,15 @@ import './checkbox.scss';
 
 export const Checkbox = ({ label }) => {
   return (
-    <div className="checkbox-wrapper">
-      <input type="checkbox" id="edit-checkbox" className="custom-control-input"/>
-      <label id="checkmark" for="edit-checkbox" name="checkboxone" className="custom-control-label">{ label }</label>
-    </div>
+    <>
+      <div className="checkbox-wrapper">
+        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" className="custom-control-input"/>
+        <label for="vehicle1" className="custom-control-label">{ label }</label>
+      </div>
+      <div className="checkbox-wrapper">
+        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" className="custom-control-input"/>
+        <label for="vehicle2" className="custom-control-label">{ label }</label>
+      </div>
+    </>
   );
 };
